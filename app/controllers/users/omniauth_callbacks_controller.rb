ce4f72b46de7a025e222d17a84e29b6b8aa00c18
kang cao <cooker_bj@hotmail.com>
2013-11-18 23:14:08 +0800

    modify the qq to qq_connect5

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,11 +18,11 @@ class Users::OmniauthCallbacksController <Devise::OmniauthCallbacksController
   end
 
   def qq_connect
-    p request.env["omniauth.auth"]
+    logger.info request.env["omniauth.auth"]
     @user = User.find_for_oauth2(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Qq"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.omniauth"] = user_hash(request.env["omniauth.auth"])

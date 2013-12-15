372bfc9bac62c0f015146901be95a9cc7c273946
kang cao <cooker_bj@hotmail.com>
2013-12-15 11:29:20 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,18 @@
                     name: "administrator" ,
                     roles: [1,2,4,8]
                  })
+    begin
+    root=Location.create({name: '北京'})
+      city=root.children.create({name: '北京'})
+        ['东城','西城','海淀','朝阳','丰台','通州','石景山','门头沟','昌平','延庆','平谷','密云','怀柔','顺义','大兴'].each do |district|
+        city.children.create({name: district})
+        end
+    end
+
+    category={'语言'=>['英语','日语','法语','德语','俄语'],'音乐'=>['钢琴','小提琴','吉他','声乐','其它'],'书画'=>['绘画','书法'],'体育'=>['游泳','轮滑','羽毛球','舞蹈','围棋','其它']}
+    category.each do |key,value|
+       root=Category.create({name: key})
+       value.each do |item|
+         root.children.create({name: item})
+       end
+    end

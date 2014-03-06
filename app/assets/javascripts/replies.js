fc4d00a4e76c9632b4e25c43cf7de881f34951cd
kang.cao <cooker_bj@hotmail.com>
2014-03-06 11:15:02 +0800

    repaired reply bug4

--- app/assets/javascripts/replies.js
+++ app/assets/javascripts/replies.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("#comment_list").on('click', '.comment_commands a[href="#"]', function(e) {
+    $("#comment_list").on('click', '.comment_commands a[href="##"]', function(e) {
         e.preventDefault();
         $("<div>").html("请点击<a href='/users/sign_in'>登录</a>回复").dialog({
             modal: true
@@ -29,7 +29,7 @@ $(function() {
     $("body").on("ajax:success", 'div.ui-dialog-content.ui-widget-content form#new_reply', function(evt, data, status, xhr) {
         var $form = $(this);
         var $cid = $(this).find("#reply_comment_id").val();
-        $form.find('input[type="text"]').val("");
+        $form.find('textarea').val("");
         var addpoint = "#reply" + $cid;
         $(addpoint).append(xhr.responseText);
     }).on("ajax:complete", 'div.ui-dialog-content.ui-widget-content form#new_reply', function(evt, status, xhr) {
@@ -38,7 +38,7 @@ $(function() {
         var handler = $(addpoint);
         var dialog = handler.data('dialog');
         dialog.dialog('close');
-    })
+    });
     $('.user').on("mouseenter", ".pop_out", function() {
         var handler = $(this);
         var p = $(this).parent().position();
